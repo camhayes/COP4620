5937651cfa891b53b262111bc82f6b034b047e0c
Liam Rigney <100379941+LimeKMS@users.noreply.github.com>
2023-01-29 21:55:57 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -4,10 +4,12 @@ import java.io.*;
 
 public class Driver {
 
+	
 	public static void main( String[] args) throws Exception 
 	{
 		
 		String fileName = "C:/Users/liamr/OneDrive/Desktop/Compilers/Step-1/Step1/inputs/fibonacci.micro"; //Add file path/name for your computer
+		//String fileName = args[0];
 		File file = new File(fileName);
 		FileInputStream fis = null;
 		
@@ -19,15 +21,30 @@ public class Driver {
 			System.exit(1);
 		}
 		
+		@SuppressWarnings("deprecation")
 		ANTLRInputStream input = new ANTLRInputStream(fis);
 
-		HelloLexer lexer = new HelloLexer(input);
+		Little lexer = new Little(input);
 
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
-
-		HelloParser parser = new HelloParser(tokens);
-		ParseTree tree = parser.r(); // begin parsing at rule 'r'               //Remove/Replace for our grammer/code
-		System.out.println(tree.toStringTree(parser)); // print LISP-style tree
+		tokens.fill();
+		
+		Vocabulary vocab = lexer.getVocabulary();
+		
+		
+		for(Object o: tokens.getTokens()) {
+			CommonToken token = (CommonToken)o;
+			System.out.println("Token Type: " + vocab.getSymbolicName(token.getType()));
+			System.out.println("Value: " + token.getText());
+			
+			
+		}
+		
+		//LittleParser parser = new LittleParser(tokens);
+		//ParseTree tree = parser(); // begin parsing at rule 'r'               //Remove/Replace for our grammer/code
+		
+		
+		System.out.println(); // print LISP-style tree
 	}
 
 }

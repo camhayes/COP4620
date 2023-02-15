0ee45c95b4c8213278dcd1aec7279ba4631c5419
Liam Rigney <100379941+LimeKMS@users.noreply.github.com>
2023-02-15 16:39:02 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,21 +1,74 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
+import java.io.*;
 
-public class Test {
-    public static void main(String[] args) throws Exception {
-        // create a CharStream that reads from standard input
-        ANTLRInputStream input = new ANTLRInputStream(System.in);
-        
-        // create a lexer that feeds off of input CharStream
-        ArrayInitLexer lexer = new ArrayInitLexer(input);
-        
-        // create a buffer of tokens pulled from the lexer
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        
-        // create a parser that feeds off the tokens buffer
-        ArrayInitParser parser = new ArrayInitParser(tokens);
-        
-        ParseTree tree = parser.init(); // begin parsing at init rule
-        System.out.println(tree.toStringTree(parser)); // print LISP-style tree
-    }
+public class Driver {
+
+	
+	public static void main( String[] args) throws Exception 
+	{
+		
+		String fileName = "C:/Users/liamr/OneDrive/Desktop/Compilers/Step-2/Step2/inputs/test2.micro"; //Add file path/name for your computer
+		//String fileName = args[0];
+		File file = new File(fileName);
+		FileInputStream fis = null;
+		
+		try {
+			fis = new FileInputStream(file);
+		}
+		catch (IOException e){
+			System.out.println("Couldn't find file");
+			System.exit(1);
+		}
+		
+		@SuppressWarnings("deprecation")
+		ANTLRInputStream input = new ANTLRInputStream(fis);
+		
+		///////////////////////////////////////////////////////////
+
+		LittleLexer lexer = new LittleLexer(input);
+
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		tokens.fill();
+		
+		Vocabulary voc = lexer.getVocabulary();
+
+		LittleParser parser = new LittleParser(tokens);
+		
+		ANTLRErrorStrategy es = new CustomErrorStrategy();
+		parser.setErrorHandler(es);
+		
+		
+		try{	
+			parser.program();
+			System.out.println("Accepted");
+		}catch(Exception e){
+			System.out.println("Not accepted");
+		}
+		
+		
+	}
+
 }
+
+class CustomErrorStrategy extends DefaultErrorStrategy{
+	@Override
+	public void recover(Parser recognizer, RecognitionException e){
+			throw e;
+	}
+	
+	@Override
+    public Token recoverInline(Parser recognizer){
+    	//return null;
+    	RecognitionException e = null;
+    	throw e;
+   	
+    }
+    
+	@Override
+	public void reportError(Parser recognizer, RecognitionException e){
+		throw e;
+	}	
+	
+}
+

83916d6525a9168e366da716dacb089073c28ff6
Cam Hayes <cmhayes81@gmail.com>
2023-02-27 15:56:23 -0500

    updated comments

--- SimpleTableBuilder.java
+++ SimpleTableBuilder.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 public class SimpleTableBuilder extends LittleBaseListener {
     @Override public void enterProgram(LittleParser.ProgramContext ctx)
     {
+        // Next:
+        // Make symbol table for global
+        // add it to the list of symbol tables
+        // push it to the scope stack
 
     }
 
@@ -13,8 +17,10 @@ public class SimpleTableBuilder extends LittleBaseListener {
         String type = "STRING";
         String value = ctx.str().getText();
         System.out.println(name + ", " + type + ", " + value);
+        // Next: Create synbol table using the information above and insert at the top of the stack
     }
+    // Next: Create supporting methods for other decl types
     public void prettyPrint() {
-        
+        // print all sumbol tables in the order they were created.
     }
 }

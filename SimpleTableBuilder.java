ba202d20dfb934dcb3ea9c6bd56fde63472d314a
Liam Rigney <100379941+LimeKMS@users.noreply.github.com>
2023-03-01 16:02:02 -0500

    Update SimpleTableBuilder.java

--- SimpleTableBuilder.java
+++ SimpleTableBuilder.java
@@ -20,6 +20,24 @@ public class SimpleTableBuilder extends LittleBaseListener {
         // Next: Create synbol table using the information above and insert at the top of the stack
     }
     // Next: Create supporting methods for other decl types
+    
+     @Override public void enterVar_decl(LittleParser.Var_declContext ctx)
+    {
+        String type = ctx.var_type().getText();
+        if (type.compareTo("INT") == 0) {
+        	String name = ctx.id_list().getText();
+        	String[] int_val = ctx.id_list().getText().split(",");
+            System.out.println(int_val[0] + ", " + type);
+        }
+        else if (type.compareTo("FLOAT") == 0) {
+        	String name = ctx.id_list().getText();
+        	String[] int_val = ctx.id_list().getText().split(",");
+            System.out.println(int_val[0] + ", " + type);
+        }
+       
+        // Next: Create symbol table using the information above and insert at the top of the stack
+    }
+    
     public void prettyPrint() {
         // print all sumbol tables in the order they were created.
     }

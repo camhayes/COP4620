6726b214b73b41f2828e6f9c9d575f8fb9f3ce8a
Liam Rigney <100379941+LimeKMS@users.noreply.github.com>
2023-03-09 18:33:59 -0500

    Add files via upload

--- SimpleTableBuilder.java
+++ SimpleTableBuilder.java
@@ -1,6 +1,87 @@
 import java.util.*;
 import java.util.ArrayList;
 
+class Symbol{
+	String name;
+	String type;
+	String value;
+	
+	public Symbol(String name, String type) {
+		this.name = name;
+		this.type = type;
+		this.value = null;
+	}
+	
+	public Symbol(String name, String type, String value) {
+		this.name = name;
+		this.type = type;
+		this.value = value;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getType() {
+		return this.type;
+	}
+	
+	public String getValue() {
+		return this.value;
+	}
+}
+
+	class SymbolTable{
+		public String scope;
+		public SymbolTable parent;
+		public ArrayList<SymbolTable> children;
+		public LinkedHashMap<String,Symbol> table;
+		
+		public SymbolTable(String scope){
+			this.scope = scope;
+			this.table = new LinkedHashMap<String,Symbol>();
+		}
+		
+		public SymbolTable getParent(){
+			return this.parent;
+		}
+		
+		public LinkedHashMap<String, Symbol> getTable(){
+			return this.table;
+		}
+		
+		public void addSymbol(Symbol symbol) throws IllegalArgumentException{
+			String name = symbol.getName();
+			String type = symbol.getType();
+			String valueString = symbol.getValue();
+			if(table.containsKey(name)) {
+				throw new IllegalArgumentException("DECLARATION ERROR " + name);
+			}
+			else {
+				table.put(name, symbol);
+			}
+		}
+		
+		public void printTable(){
+			System.out.println("Symbol table "+this.scope);
+			Iterator<Symbol> symbols = table.values().iterator();
+			while(symbols.hasNext()){
+				Symbol currentSymbol = symbols.next();
+				String name = currentSymbol.getName();
+				String type = currentSymbol.getType();
+				String value = currentSymbol.getValue();
+				
+				if(type.compareTo("STRING") == 0) {
+					System.out.println("name " + name + " type " + type + " value " + value);
+				}
+				else {
+					System.out.println("name " + name + " type " + type);
+				}
+			}
+		}
+	}
+
+
 public class SimpleTableBuilder extends LittleBaseListener {
     @Override public void enterProgram(LittleParser.ProgramContext ctx)
     {
@@ -10,16 +91,18 @@ public class SimpleTableBuilder extends LittleBaseListener {
         // push it to the scope stack
 
     }
+    
+    SymbolTable table = new SymbolTable("name");
 
     @Override public void enterString_decl(LittleParser.String_declContext ctx)
     {
         String name = ctx.id().getText();
         String type = "STRING";
         String value = ctx.str().getText();
-        System.out.println(name + ", " + type + ", " + value);
-        // Next: Create synbol table using the information above and insert at the top of the stack
+        Symbol symbol = new Symbol(name, type, value);
+        table.addSymbol(symbol);
+        // Next: Create symbol table using the information above and insert at the top of the stack
     }
-    // Next: Create supporting methods for other decl types
     
     @Override public void enterVar_decl(LittleParser.Var_declContext ctx)
     {
@@ -28,21 +111,27 @@ public class SimpleTableBuilder extends LittleBaseListener {
         	String name = ctx.id_list().getText();
         	String[] int_val = ctx.id_list().getText().split(",");
         	for(int i = 0;i < int_val.length; i++) {
-        		System.out.println(int_val[i] + ", " + type);
+        		Symbol symbol = new Symbol(int_val[i], type);
+        		table.addSymbol(symbol);
         	}
         }
         else if (type.compareTo("FLOAT") == 0) {
         	String name = ctx.id_list().getText();
         	String[] int_val = ctx.id_list().getText().split(",");
         	for(int i = 0;i < int_val.length; i++) {
-        		System.out.println(int_val[i] + ", " + type);
+        		Symbol symbol = new Symbol(int_val[i], type);
+        		table.addSymbol(symbol);
         	}
         }
        
         // Next: Create symbol table using the information above and insert at the top of the stack
     }
     
+    
+    
+    // Next: Create supporting methods for other decl types
     public void prettyPrint() {
-        // print all sumbol tables in the order they were created.
+    	table.printTable();
+        // print all symbol tables in the order they were created.
     }
 }

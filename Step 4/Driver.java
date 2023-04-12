42b5b83d9769bbca52e35d63fffe2dd362566ff1
Cam Hayes <cmhayes81@gmail.com>
2023-04-12 19:49:01 -0400

    big init - AST base
    Adds in the ability to construct an AST that currently recognizes simple expression and assignments.

---
+++ Step 4/Driver.java
@@ -0,0 +1,31 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+import java.io.*;
+
+public class Driver {
+
+
+    public static void main( String[] args) throws Exception
+    {
+        ANTLRInputStream input = new ANTLRInputStream(System.in);
+
+        LittleLexer lexer = new LittleLexer(input);
+
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        LittleParser parser = new LittleParser(tokens);
+
+        ParseTree tree = parser.program();
+
+        ParseTreeWalker walker = new ParseTreeWalker();
+
+        ASTBuilder ast = new ASTBuilder();
+
+        walker.walk(ast, tree);
+
+        ast.prettyPrint();
+
+    }
+
+}
+
